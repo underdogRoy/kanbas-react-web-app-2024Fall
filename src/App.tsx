b39f4fd38d5f5bf72dcbb392ecad06128c7dfd8d
RoyBao96 <baoruyi.hc07@gmail.com>
2024-09-08 21:22:46 -0700

    a1 html fa24

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Lab from './Lab.tsx';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Web Dev</h1>
+      <Lab />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Lab3.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Lab3(){
+    return <div><h2>Lab3</h2></div>
+}
